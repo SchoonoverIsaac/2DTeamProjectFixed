63f7fb6b76f59fc3235dcb14cc4d2395788eea56
Siege-Xcalibur <27.israel.olivera@ksd.org>
2025-11-14 12:45:47 -0800

    BossManRelatedStuff

---
+++ 2D Team Project/Assets/Scenes/Scripts/BossManAnimControls.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManAnimControls : MonoBehaviour
+{
+    Animator animator;
+    SpriteRenderer SPR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        SPR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = GetComponent<Rigidbody2D>().velocity.x;
+        animator.SetFloat("x", moveX);
+        if (moveX > 0)
+        {
+            //we're moving to the left
+            //flip our sprite
+            SPR.flipX = true;
+        }
+        else if (moveX < 0)
+        {
+            SPR.flipX = false;
+        }
+    }
+}
